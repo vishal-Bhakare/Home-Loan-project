862d82920c0dcca25903a0da188bc1b6c488cfd5
vishal <vishalbhakare1998@gmail.com>
2023-05-17 14:16:25 +0530

    code remove

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
